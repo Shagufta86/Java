acf4beaad2d068434cd1b705aee830f3db3350bd
Shagufta Mahmood <103606378+Shagufta86@users.noreply.github.com>
2023-09-26 21:44:58 +0530

    Update Operators.java

---
+++ Operators.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class Operators {
+    public static void main(String[] args) {
+
+    // int a = 4;
+    //int b = 6 % a;
+    int b =9;
+    b *= 3;
+        System.out.println(b);
+        System.out.println(64>4 & 6>5);
+
+        /////// PRECEDENCE /////
+   /*int a = 6;
+    int b = 2;
+    int c = 4;
+    int k = b*b-(4*a*c)/(2*a);
+    System.out.println(k);*/
+
+    //Incremrnt & Decrement Operators
+    int i = 56;
+    System.out.println(i++);
+    System.out.println(i);
+    System.out.println(++i);
+    System.out.println(i);
+   }
+}
