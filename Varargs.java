5b62ae25d86d1acc35cb462d189cff970d41fe27
Shagufta Mahmood <103606378+Shagufta86@users.noreply.github.com>
2023-09-30 23:29:12 +0530

    Update Varargs.java

--- Varargs.java
+++ Varargs.java
@@ -1,5 +1,30 @@
 package Java;
 
 public class Varargs {
+  static int product(int...arr){
+        int result=1;
+        for(int a:arr){
+            result *= a;
+        }
+        return result;
+    }
+   public static void main(String[] args) {
+       System.out.println("The product of 4 ,5 and 9 is: "+ product(4,5,9));
+    } 
+   }
 
-}
+
+   /////////  Find average of given number in method  //////////
+/*static float average(float...marks){
+       float total=0;
+       for (float scan:marks){
+           total+=scan;
+       }
+       System.out.println(total/ marks.length);
+       return total;
+   }
+
+   public static void main(String[] args) {
+
+       float sum=average(1,2,3,4);
+*/
